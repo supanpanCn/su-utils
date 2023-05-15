57a8af94327bb2a748cb40239a1cc29e0e2824d9
spp <1372978934@qq.com>
2023-05-15 16:31:52 +0800

    新增scanDir

--- src/node.ts
+++ src/node.ts
@@ -1,5 +1,7 @@
 import { resolveModule } from "local-pkg";
 import parseCode from "./parse";
+import { readdirSync,statSync } from "node:fs"
+import { resolve } from 'node:path'
 
 function _dirname(pkgName: string) {
   const entry = resolveModule(pkgName, {
@@ -10,7 +12,22 @@ function _dirname(pkgName: string) {
   }
 }
 
+function scanDir(entry: string, res: string[]) {
+  const files = readdirSync(entry);
+  for (let file of files) {
+    const full = resolve(entry, file);
+    const stat = statSync(full);
+    if (stat.isFile()) {
+      res.push(full);
+      continue;
+    }
+    scanDir(full, res);
+  }
+  return res;
+}
+
 export {
   parseCode,
-  _dirname
+  _dirname,
+  scanDir
 }
